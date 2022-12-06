daf2b6ae79fec2424a9b9c4bbc5d4381d8caaca4
Andres <nanoandres_24@hotmail.com>
2022-12-06 17:31:51 +0100

    fixed n-gram dictionary

--- my_functions.py
+++ my_functions.py
@@ -28,31 +28,10 @@ def create_decoder_ngram():
     """Create a decoder based on the Ngram language model"""
     config = Decoder.default_config()
     config.set_string('-hmm', 'ps_data/model/en-us')  # acoustic model
-    config.set_string('-dict', 'ps_data/lex/turtle.dict')  # lexicon / dictionary
-    config.set_string('-lm', 'ps_data/lm/turtle.lm.bin')  # language model
+    config.set_string('-dict', 'ps_data/lex/digits.dict')  # lexicon / dictionary
+    config.set_string('-lm', 'ps_data/lm/en-us.lm.bin')  # language model
     decoder_ngram = Decoder(config)
     return decoder_ngram
-
-
-def create_decoder_goforward():
-    """Create a decoder based on the goforward custom grammar"""
-    config = Decoder.default_config()
-    config.set_string('-hmm', 'ps_data/model/en-us')  # acoustic model
-    config.set_string('-dict', 'ps_data/lex/turtle.dict')  # lexicon / dictionary
-    decoder_gofwd = Decoder(config)
-
-    # Now we use a custom language model
-    # Prepare the grammar to be used
-    jsgf = Jsgf('ps_data/jsgf/goforward.jsgf')  # load the grammar file
-    rule = jsgf.get_rule('goforward.move2')  # choose the rule
-    fsg = jsgf.build_fsg(rule, decoder_gofwd.get_logmath(), 7.5)  # build the grammar rule
-    fsg.writefile('outputs/goforward.fsg')  # write the compiled grammar rule as an external file
-
-    # Now set the fsg grammar rule in the decoder
-    decoder_gofwd.set_fsg("outputs/goforward", fsg)  # load the pre-recorded compiled grammar rule in the decoder
-    decoder_gofwd.set_search("outputs/goforward")  # and set it as the grammar to use
-
-    return decoder_gofwd
 
 
 def create_decoder_digit(rule):
